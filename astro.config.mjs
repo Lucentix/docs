369d07031c8dd94cd6c1528b4f96f3eb4abbadff
dev-lucr4tiv3 <dev_lucr4tiv3d@outlook.com>
2025-02-23 15:22:23 +0100

    updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,30 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
 export default defineConfig({
-	site: 'https://lucentix.github.io',
-	base: '/docs',
-	integrations: [
-	  starlight({
-		title: 'My Docs',
-		social: {
-		  github: 'https://github.com/withastro/starlight',
-		},
-		sidebar: [
-		  {
-			label: 'Guides',
-			items: [
-			  { label: 'Example Guide', slug: 'guides/example' },
-			],
-		  },
-		  {
-			label: 'Reference',
-			autogenerate: { directory: 'reference' },
-		  },
-		],
-	  }),
-	],
-	images: {
-	  service: 'pass-through',
-	},
-  });
-  
+  site: 'https://lucentix.github.io',
+  base: '/docs',
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+  images: {
+    service: 'pass-through',
+  },
+});
